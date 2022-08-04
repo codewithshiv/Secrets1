f193a28a010893d10fedace4e7b1494f481772b6
SHIVENDU KUMAR MANDAL <shivamkumarr675@gmail.com>
2022-08-04 12:42:48 +0530

    Level 3 .env added with .gitigonr.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 const encrypt = require('mongoose-encryption')
 const app = express()
+
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -21,8 +25,7 @@ const userSchema = new mongoose.Schema({
 //     password: String
 // }
 
-const secret = "This is a Secret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
